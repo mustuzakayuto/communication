b2625f4cbefe91d1d9802a93957a6c03b79427cc
松坂侑隼 <220063ie@yse-c.net>
2023-08-28 08:56:18 +0900

    AIとコミュニケーション

--- static/js/ai_chat.js
+++ static/js/ai_chat.js
@@ -19,3 +19,26 @@ function chat_transmission(){
 
 
 transmission.addEventListener('click', chat_transmission);
+
+
+const speech = new webkitSpeechRecognition();
+speech.lang = 'ja-JP';
+
+const btn = document.getElementById('btn');
+const content = document.getElementById('content');
+
+btn.addEventListener('click' , function() {
+    speech.start();
+});
+
+speech.onresult = function(e) {
+        speech.stop();
+        if(e.results[0].isFinal){
+            var autotext =  e.results[0][0].transcript
+            content.innerHTML += '<div>'+ autotext +'</div>';
+        }
+    }
+
+    speech.onend = () => { 
+    speech.start() 
+    };
