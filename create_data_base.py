240c828babbfc6a5fbc353b43edf479cf111b02a
松坂侑隼 <220063ie@yse-c.net>
2023-10-30 12:09:47 +0900

    変更

--- create_data_base.py
+++ create_data_base.py
@@ -2,10 +2,12 @@ from data.create import create_data_base_chat
 from data.create import create_data_base_face
 from data.create import create_db
 from data.create import create_data_base_faceaverage
+from data.create import create_data_base_image
 from data.control import setchat
 
 create_data_base_chat.main()
 create_data_base_face.main()
 create_data_base_faceaverage.main()
+create_data_base_image.main()
 create_db.main()
 setchat.create_room_all()
--- ico変換.py
+++ ico変換.py
@@ -1,12 +1,16 @@
 from PIL import Image
-static="static/"
-# PNG画像　ファイル名
-PNG_FILE_NAME = static+"sample.jpg"
-# ICO画像　ファイル名
-ICO_FILE_NAME = static+"sample.ico"
- 
-# PNG画像を開く
-img = Image.open(PNG_FILE_NAME)
- 
-# ICO画像保存
-img.save(ICO_FILE_NAME, format="ICO", sizes=[(132, 132)])
+def main():
+    static="static/"
+    # PNG画像　ファイル名
+    # PNG_FILE_NAME = static+"sample.jpg"
+    PNG_FILE_NAME = static+input("ファイルネーム(拡張子必須):")
+    # ICO画像　ファイル名
+    ICO_FILE_NAME = static+"sample.ico"
+    
+    # PNG画像を開く
+    img = Image.open(PNG_FILE_NAME)
+    
+    # ICO画像保存
+    img.save(ICO_FILE_NAME, format="ICO", sizes=[(132, 132)])
+if __name__ =="__main__":
+    main()
